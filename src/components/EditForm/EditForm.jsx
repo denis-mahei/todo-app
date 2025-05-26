2b97f31e087ddfdfde22fc6c955971f207b1a922
denis-mahei <denmahei@gmail.com>
2025-05-26 16:45:06 +0300

    update EditForm

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -5,21 +5,46 @@ import style from './EditForm.module.css';
 import { useTodoStore } from '../../store/useTodoStore.js';
 
 const EditForm = () => {
+  const { setCurrentTodo, editTodo, currentTodo } = useTodoStore(state => ({
+    currentTodo: state.currentTodo,
+    editTodo: state.editTodo,
+    setCurrentTodo: state.setCurrentTodo,
+  }));
+
+  if (!currentTodo) return null;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const form = e.target;
+    const newText = form.elements.text.value;
+    if (!newText) return;
+
+    editTodo(currentTodo.id, { text: newText });
+    setCurrentTodo(null);
+
+    form.reset();
+  };
+
+  const handleCancel = () => {
+    setCurrentTodo(null);
+  };
+
   return (
-    <form className={style.form} onSubmit={{}}>
+    <form className={style.form} onSubmit={handleSubmit}>
       <input
         className={style.input}
         placeholder="What do you want to write?"
         name="text"
         required
-        defaultValue={''}
+        defaultValue={currentTodo.text}
         autoFocus
       />
       <button className={style.submitButton} type="submit">
         <RiSaveLine color="green" size="16px" />
       </button>
 
-      <button className={style.editButton} type="button" onClick={{}}>
+      <button className={style.editButton} type="button" onClick={handleCancel}>
         <MdOutlineCancel color="red" size="16px" />
       </button>
     </form>
